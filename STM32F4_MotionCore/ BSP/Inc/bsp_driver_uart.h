f6464b1a765cde6ccd968ce70595de303440520b
MaysStar <123299437+MaysStar@users.noreply.github.com>
2026-04-06 12:54:25 +0300

    Fix incorrect structure resolution after merge conflict

--- STM32F4_MotionCore/ BSP/Inc/bsp_driver_uart.h
+++ STM32F4_MotionCore/ BSP/Inc/bsp_driver_uart.h
@@ -35,18 +35,18 @@ DevStatus_t BSP_UART_1Wire_RegisterTxCpltCallbak(void (*callback_fun)(void));
 DevStatus_t BSP_UART_1Wire_RegisterRxCpltCallbak(void (*callback_fun)(void));
 
 /* BSP for 1Wire */
-void BSP_UART_1WireDS18B20_Init(UART_HandleTypeDef* huart1);
-int BSP_UART_1WireDS18B20_ResetPresence(void);
+DevStatus_t BSP_UART_1WireDS18B20_Init(UART_HandleTypeDef* huart1);
+DevStatus_t BSP_UART_1WireDS18B20_ResetPresence(void);
 
 /* Write,read and calculate block */
-void BSP_UART_1WireDS18B20_Write(uint8_t data);
-void BSP_UART_1WireDS18B20_ReadStart(void);
+DevStatus_t BSP_UART_1WireDS18B20_Write(uint8_t data);
+DevStatus_t BSP_UART_1WireDS18B20_ReadStart(void);
 uint8_t BSP_UART_1WireDS18B20_ReadEnd(void);
 
 uint8_t BSP_UART_1WireDS18B20_CalculateCRC(uint8_t* data, uint32_t len);
 
 /* Register callback */
-void BSP_UART_1Wire_RegisterTxCpltCallbak(void (*callback_fun)(void));
-void BSP_UART_1Wire_RegisterRxCpltCallbak(void (*callback_fun)(void));
+DevStatus_t BSP_UART_1Wire_RegisterTxCpltCallbak(void (*callback_fun)(void));
+DevStatus_t BSP_UART_1Wire_RegisterRxCpltCallbak(void (*callback_fun)(void));
 
 #endif /* INC_BSP_DRIVER_UART_H_ */
